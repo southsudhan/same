0311f6bf16bff2f2e4e192f71ca7f97de8023c1d
Amin-Kei <moakeimasi@gmail.com>
2024-12-31 20:15:12 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button>hello vite</button>
+      <h1>hello Martin What go in on ?</h1>
+    </div>
+  );
+}
+
+export default App;
