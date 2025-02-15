464f15d4ea989160f9988628e7f0a6bb8208e170
Amin-Kei <moakeimasi@gmail.com>
2025-02-15 16:45:52 +0330

    add routhing to main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import TradeRoom from "./Pages/TradeRoom/TradeRoom";
+import Market from "./Pages/Market/Market";
+import Wallet from "./Pages/Wallet/Wallet";
+import SignUp from "./Pages/SignUp/SignUp";
+import Playground from "./Playground/Playground";
+// import { useCurrencies } from "./Hooks/Currencies/useCurrencies";
+import NavigationBar from "./Components/NavigationBar/NavigationBar";
+// import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./store.ts";
@@ -12,7 +21,20 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        {/* <App /> */}
+        <div className="w-screen h-screen">
+      <NavigationBar />
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/traderoom" element={<TradeRoom />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/wallet" element={<Wallet />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/playground" element={<Playground />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
       </QueryClientProvider>
     </Provider>
   </StrictMode>
