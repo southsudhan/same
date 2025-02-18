a76d1fa05c43b1f47b41fe78e4226d012bbb67de
Amin-Kei <moakeimasi@gmail.com>
2025-02-18 13:42:10 +0330

    fix routhing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import TradeRoom from "./Pages/TradeRoom/TradeRoom";
-import Market from "./Pages/Market/Market";
-import Wallet from "./Pages/Wallet/Wallet";
-import SignUp from "./Pages/SignUp/SignUp";
-import Playground from "./Playground/Playground";
+// import TradeRoom from "./Pages/TradeRoom/TradeRoom";
+// import Market from "./Pages/Market/Market";
+// import Wallet from "./Pages/Wallet/Wallet";
+// import SignUp from "./Pages/SignUp/SignUp";
+// import Playground from "./Playground/Playground";
 // import { useCurrencies } from "./Hooks/Currencies/useCurrencies";
-import NavigationBar from "./Components/NavigationBar/NavigationBar";
+// import NavigationBar from "./Components/NavigationBar/NavigationBar";
 
 function App() {
   // const { data, error, isLoading } = useCurrencies();
 
   return (
     <div className="w-screen h-screen">
-      <NavigationBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/traderoom" element={<TradeRoom />} />
-          <Route path="/market" element={<Market />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/playground" element={<Playground />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./store.ts";
+import NavigationBar from "./Components/NavigationBar/NavigationBar.tsx";
+import Home from "./Pages/Home/Home.tsx";
+import TradeRoom from "./Pages/TradeRoom/TradeRoom.tsx";
+import Market from "./Pages/Market/Market.tsx";
+import Wallet from "./Pages/Wallet/Wallet.tsx";
+import SignUp from "./Pages/SignUp/SignUp.tsx";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +19,18 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        {/* <App /> */}
+        <NavigationBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/traderoom" element={<TradeRoom />} />
+            <Route path="/market" element={<Market />} />
+            <Route path="/wallet" element={<Wallet />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* <Route path="/playground" element={<Playground />} /> */}
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </Provider>
   </StrictMode>
