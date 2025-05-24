6e5f225ad86b0d1f74aef74b7c893ca18a38ad0a
southsudhan <143240686+southsudhan@users.noreply.github.com>
2025-05-24 03:53:24 +0100

    Set up Auth0 provider and fix imports

---
+++ src/auth/Auth0ProviderWithConfig.tsx
@@ -0,0 +1,23 @@
+import { Auth0Provider } from '@auth0/auth0-react';
+
+export const Auth0ProviderWithConfig = ({ children }: { children: React.ReactNode }) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  if (!(domain && clientId && redirectUri)) {
+    return null;
+  }
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { store } from "./store.ts";
-import { Auth0ProviderWithConfig } from "./auth/Auth0ProviderWithConfig.tsx";
+import { Auth0ProviderWithConfig } from "./auth/Auth0ProviderWithConfig";
 import NavigationBar from "./Components/NavigationBar/NavigationBar.tsx";
 import Home from "./Pages/Home/Home.tsx";
 import TradeRoom from "./Pages/TradeRoom/TradeRoom.tsx";
